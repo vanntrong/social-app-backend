43e618a27b1dd32b599abe8b249f24fc6134943b
vanntrong <vantrongbrv@gmail.com>
2023-01-08 14:37:01 +0700

    feat: upadte cors

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ const app = express();
 const httpServer = createServer(app);
 
 //middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://social-app.vovantrong.online"],
+  })
+);
 app.use(morgan("short"));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
@@ -59,7 +63,7 @@ httpServer.listen(PORT, (e) => {
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://46.137.203.198:3000", "https://46.137.203.198:3000"],
+    origin: ["https://social-app.vovantrong.online"],
   },
 });
 
